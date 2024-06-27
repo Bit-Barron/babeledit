cc00776b7f008c1fbc87a2f149f49004f7003277
Bit-Barron <azerabdu5@gmail.com>
2024-06-27 18:45:00 +0200

    asd

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,7 +5,6 @@ import icon from '../../resources/icon.png?asset'
 import fs from 'fs'
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -27,8 +26,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -36,63 +33,38 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
   ipcMain.on('ping', () => console.log('pondasdasdg'))
 
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-ipcMain.on('open-file', (event) => {
-  dialog
-    .showOpenDialog({
-      properties: ['openFile'],
-      filters: [{ name: 'JSON FILES', extensions: ['json'] }]
-    })
-    .then((result) => {
-      if (!result.canceled) {
-        const filePath = result.filePaths[0]
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-          if (err) {
-            console.log('Error')
-            return
-          }
-          event.sender.send('selected-file', data)
-        })
-      }
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+ipcMain.on('open-file', async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'JSON Files', extensions: ['json'] }]
+  })
+
+  if (canceled) {
+    return { canceled: true }
+  }
+  const fileContent = fs.readFileSync(filePaths[0], 'utf-8')
+  console.log(fileContent)
+  return { canceled: false, content: JSON.parse(fileContent) }
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
