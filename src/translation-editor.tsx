21e3a232bdf3381625d56a804ce1bb5eec58111d
Bit-Barron <bit.barron22@gmail.com>
2024-11-18 21:52:11 +0100

    clean up

---
+++ src/translation-editor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TranslationEditorProps {}
+
+export const TranslationEditor: React.FC<TranslationEditorProps> = ({}) => {
+  return (
+    <section>
+      <h1>Translation Editor</h1>
+    </section>
+  );
+};
