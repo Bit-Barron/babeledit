10bc2087e1737812f4a5b11bbb1d030d0a4884aa
Bit-Barron <bit.barron22@gmail.com>
2024-11-18 12:26:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Welcome to Tauri + React</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input id="greet-input" placeholder="Enter a name..." />
+        <button type="submit">Greet</button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
