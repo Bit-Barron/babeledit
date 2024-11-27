8f608263bee75afca258daab91046e87a587aeae
bit-barron <bit.barron22@gmail.com>
2024-11-27 22:39:18 +0100

    easy

--- src/components/pages/translation-editor/translation-content.tsx
+++ src/components/pages/translation-editor/translation-content.tsx
@@ -5,6 +5,7 @@ import { highlightPlaceholders } from "@/utils/client-helper";
 import { Separator } from "@/components/ui/separator";
 import { useLanguageStore } from "@/store/language-store";
 import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
 
 interface TranslationContentProps {
   node: TreeNode | null;
@@ -15,6 +16,35 @@ export const TranslationContent: React.FC<TranslationContentProps> = ({
 }) => {
   const { languages } = useLanguageStore();
   const nodeLanguages = node?.content ? Object.entries(node.content) : [];
+  const [translations, setTranslations] = useState<Record<string, string>>({});
+
+  useEffect(() => {
+    const translateContent = async () => {
+      if (!languages.length || !nodeLanguages.length) return;
+      const newTranslations: Record<string, string> = {};
+
+      for (const [lang, content] of nodeLanguages) {
+        if (!content) continue;
+
+        const sourceLang = lang.split("-")[0];
+
+        for (const language of languages) {
+          const response = await fetch(
+            `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
+              content
+            )}&langpair=${sourceLang}|${language.name}`
+          );
+
+          if (response.ok) {
+            const { responseData } = await response.json();
+            newTranslations[language.name] = responseData.translatedText;
+          }
+        }
+      }
+      setTranslations(newTranslations);
+    };
+    translateContent();
+  }, [node]);
 
   return (
     <section>
@@ -32,7 +62,7 @@ export const TranslationContent: React.FC<TranslationContentProps> = ({
             return (
               <div key={lang}>
                 <div className="flex items-center gap-4">
-                  <label className="text-sm text-gray-400 min-w-[100px]">
+                  <label className="text-sm text-gray-400 min-w-[50px]">
                     {cleanedLang}
                   </label>
                   {highlightPlaceholders(content || "")}
@@ -48,13 +78,16 @@ export const TranslationContent: React.FC<TranslationContentProps> = ({
         <div className="p-2">
           <Separator className="mt-5" orientation="horizontal" />
           <div className="space-y-4 mt-5">
-            {languages.map((language) => (
-              <div key={language.id}>
+            {languages.map((language, idx) => (
+              <div key={idx}>
                 <div className="flex items-center gap-4">
-                  <label className="text-sm text-gray-400 min-w-[100px]">
+                  <label className="text-sm text-gray-400 min-w-[50px]">
                     {language.name}
                   </label>
-                  <Input className="flex-1" />
+                  <Input
+                    className="flex-1"
+                    value={translations[language.name] || ""}
+                  />
                   <div className="flex items-center gap-2">
                     <Checkbox />
                     <span>Approved</span>
