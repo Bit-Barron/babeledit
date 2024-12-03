07ef0c847e398a5634feb5f559bc6314e2c75a89
Bit-Barron <bit.barron22@gmail.com>
2024-12-03 19:57:40 +0100

    easy

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import "./app.css";
 import { HomeView } from "@/components/pages/home/home-view";
 import { TranslationEditor } from "@/translation-editor";
-import "./fonts.css";
 
 function App() {
   const [isCreateProjectOpen, setIsCreateProjectOpen] =
